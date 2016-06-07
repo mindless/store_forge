88c5d6ca0b7c9d02e471232a19a07c9861372e95
Prelang Builder <builder@prelang.com>
2016-06-07 07:17:49 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'SalesOrder' to allow for create, update

--- app/admin/sales_order.rb
+++ app/admin/sales_order.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register SalesOrder do
+  permit_params :sub_sales_channel, :order_number, :email, :shipping_addressee, :shipping_address_line_1, :shipping_address_line_2, :shipping_address_city, :shipping_address_state_province, :shipping_address_country, :shipping_address_postal_code, :shipping_address_phone, :pickup_addressee, :pickup_address, :pickup_phone, :item_qty, :gross_total, :payment_type, :shipping_type
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
